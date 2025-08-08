db9ce42a565bfc3bb7ff326078d1ae92847738b5
LISTED MILE <listedmile@gmail.com>
2025-08-08 11:20:41 +0530

    GO

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // true in prod (HTTPS only)
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      sameSite: "lax",
     }
   })
 );
