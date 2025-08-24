06bc76ec46e3b9f3fdc97bd55d1dfe567d0d17ca
LISTED MILE <listedmile@gmail.com>
2025-08-24 15:54:52 +0530

    storeJobList responsive

--- frontend/src/components/pages/store/storeJobList.jsx
+++ frontend/src/components/pages/store/storeJobList.jsx
@@ -147,22 +147,25 @@ export default function StoreJobList() {
     <div className="w-full min-h-[100vh] flex flex-col items-center z-[">
       <div className=" fixed h-[100vh] w-[100vw] top-0 left-0 bg-gradient-to-b from-black via-[#042029] to-[#060a13] z-[-10]"></div>
       <NavHome />
-      <h1 className="text-3xl font-bold my-6 text-white text-center">
-        Vacancies
+      <h1 className="relative text-3xl w-full py-4 font-bold text-white text-center">
+        <span className="relative z-10">Vacancies</span>
+        <span className="absolute inset-0 z-0 bg-gradient-to-r from-transparent via-cyan-500/20 to-transparent animate-shimmer"></span>
       </h1>
       {jobs.length === 0 && <Empty />}
       <div className=" w-full sm:w-[80%] ">
-        <ul className="gap-8 mt-12 flex flex-col items-center w-full ">
+        <ul className="gap-8 mt-12 flex flex-col sm:flex-row flex-wrap justify-around items-center w-full ">
           {jobs.map((job) => (
             <li
               key={job._id}
-              className="bg-[rgb(0,16,19)] flex gap-12 flex-col  border-white shadow-md  wrap-break-word rounded-lg p-6 w-[70%]"
+              className="bg-white/5 backdrop-blur-md border border-white/10 
+           rounded-2xl shadow-lg hover:shadow-cyan-500/20 
+           transition transform hover:scale-[1.02] flex gap-12 flex-col    wrap-break-word p-6 w-[95%] sm:w-[400px] "
             >
-              <div className="flex justify-between items-center text-3xl pr-8">
+              <div className="flex justify-between items-center border-b pb-3 border-white text-3xl pr-4">
                 <span></span>
 
                 {job.applied == true && (
-                  <div className="mt-2 flex gap-3">
+                  <div className=" flex items-center gap-3">
                     <label className=" text-gray-400 text-xl">Status:</label>
                     <p className="text-white text-xl">{job.status}</p>
                   </div>
@@ -176,64 +179,65 @@ export default function StoreJobList() {
                   {job.fav ? "★" : "☆"}
                 </button>
               </div>
-             <div className="w-full flex flex-col gap-4 pl-16">
-                  <h2 className="text-3xl text-cyan-400 font-semibold">
-                    {job.jobPost}
-                  </h2>
 
-                  <div className="mt-2 flex gap-3">
-                    <label className=" text-gray-400 text-xl">
-                      Organization:
-                    </label>
-                    <p className="text-white text-xl">{job.jobCompany}</p>
-                  </div>
+              <div className="w-full flex flex-col gap-4">
+                <h2 className="text-3xl self-center text-cyan-400 font-semibold">
+                  {job.jobPost}
+                </h2>
 
-                  <div className="mt-2 flex gap-3">
-                    <label className=" text-gray-400 font-medium">
-                      Location:
-                    </label>
-                    <p className="text-white">{job.jobLocation}</p>
-                  </div>
-
-                  <div className="mt-2 flex gap-3">
-                    <label className=" text-gray-400 font-medium">
-                      Salary Offered:
-                    </label>
-                    <p className="text-cyan-300">{job.jobSalaryOffered}</p>
-                  </div>
-
-                  <div className="mt-2 flex gap-3">
-                    <label className=" text-gray-400 font-medium">
-                      Required Experience:
-                    </label>
-                    <p className="text-white">{job.jobExperienceRequired}</p>
-                  </div>
-
-                  <div className="mt-4 flex gap-3 justify-end items-center pr-4">
-                    <Link
-                      to={`/store/storeJobDetails/${job._id}`}
-                      className="bg-teal-600 text-white hover:bg-teal-800 px-4 py-2 rounded-lg  "
-                    >
-                      Details..
-                    </Link>
-                    <Link
-                      to={`/store/aboutRecruiter/${job.jobUploader}`}
-                      className="bg-cyan-600 text-white hover:bg-cyan-800 px-4 py-2  rounded-lg  "
-                    >
-                      Uploader Profile
-                    </Link>
-
-                    <Link
-                      to={`/store/storeOffererJobs/${job.jobUploader}`}
-                      className="bg-teal-600 text-white hover:bg-teal-800 px-4 py-2  rounded-lg  "
-                    >
-                      Get Uploaded Jobs
-                    </Link>
-                  </div>
+                <div className="mt-2 flex gap-3">
+                  <label className=" text-gray-400 text-xl">
+                    Organization:
+                  </label>
+                  <p className="text-white text-xl">{job.jobCompany}</p>
                 </div>
-        
 
-              <div className="mt-8 items-center gap-3 pr-4 flex justify-end">
+                <div className="mt-2 flex gap-3">
+                  <label className=" text-gray-400 font-medium">
+                    Location:
+                  </label>
+                  <p className="text-white">{job.jobLocation}</p>
+                </div>
+
+                <div className="mt-2 flex gap-3">
+                  <label className=" text-gray-400 font-medium">
+                    Salary Offered:
+                  </label>
+                  <p className="text-cyan-300">{job.jobSalaryOffered}</p>
+                </div>
+
+                <div className="mt-2 flex gap-3">
+                  <label className=" text-gray-400 font-medium">
+                    Required Experience:
+                  </label>
+                  <p className="text-white">{job.jobExperienceRequired}</p>
+                </div>
+
+                <Link
+                  to={`/store/storeJobDetails/${job._id}`}
+                  className="bg-teal-600 text-white hover:bg-teal-800 px-4 py-2 rounded-lg  "
+                >
+                  Details..
+                </Link>
+
+                <div className="mt-2 flex gap-3 justify-start items-center pr-4">
+                  <Link
+                    to={`/store/aboutRecruiter/${job.jobUploader}`}
+                    className="bg-cyan-600 text-white hover:bg-cyan-800 px-4 py-2  rounded-lg  "
+                  >
+                    Uploader Profile
+                  </Link>
+
+                  <Link
+                    to={`/store/storeOffererJobs/${job.jobUploader}`}
+                    className="bg-teal-600 text-white hover:bg-teal-800 px-4 py-2  rounded-lg  "
+                  >
+                    Get Uploaded Jobs
+                  </Link>
+                </div>
+              </div>
+
+              <div className="mt-4 items-center gap-3 pr-4 flex justify-end">
                 <button
                   onClick={() => handleApply(job._id)}
                   className=" w-fit bg-cyan-800 text-white py-2 px-4 rounded hover:bg-teal-950 transition"
