f3e6e65ce87bdc6c8d2f33053a299f692c231023
LISTED MILE <listedmile@gmail.com>
2025-08-24 15:55:12 +0530

    storeProfileList responsive

--- frontend/src/components/pages/store/storeProfileList.jsx
+++ frontend/src/components/pages/store/storeProfileList.jsx
@@ -56,21 +56,26 @@ export default function StoreProfilesList() {
   };
 
   return (
-    <div className="w-full min-h-[100vh] text-white bg-black flex flex-col items-center">
+    <div className="w-full min-h-[100vh] flex flex-col items-center z-[">
+      <div className=" fixed h-[100vh] w-[100vw] top-0 left-0 bg-gradient-to-b from-black via-[#042029] to-[#060a13] z-[-10]"></div>
       <NavHome />
-
-      <h1 className="text-3xl font-bold text-center my-4">Your Resumes</h1>
+      <h1 className="relative text-3xl w-full py-4 font-bold text-white text-center">
+        <span className="relative z-10">Your Resumes</span>
+        <span className="absolute inset-0 z-0 bg-gradient-to-r from-transparent via-cyan-500/20 to-transparent animate-shimmer"></span>
+      </h1>
 
       {profiles.length === 0 && <Empty />}
 
-      <div className="w-[70%] pb-12">
-        <ul className="gap-8 mt-12 flex flex-col items-center w-full ">
+      <div className=" w-full sm:w-[80%] ">
+        <ul className="gap-8 mt-12 flex flex-col sm:flex-row flex-wrap justify-around items-center w-full ">
           {profiles.map((detail) => (
             <li
               key={detail._id}
-              className="bg-[#0d212e80] rounded-2xl shadow-md p-6 flex flex-col w-full justify-between "
+              className="bg-white/5 backdrop-blur-md border border-white/10 
+           rounded-2xl shadow-lg hover:shadow-cyan-500/20 
+           transition transform hover:scale-[1.02] flex gap-12 sm:gap-6 flex-col    wrap-break-word p-6 w-[95%] sm:w-[400px] "
             >
-              <div className="flex justify-end items-center text-2xl gap-12 pr-8">
+              <div className="flex justify-end gap-4 items-center border-b pb-3 border-white text-3xl pr-4">
                 <Link
                   to={`/store/addProfile/${detail._id}?editing=true`}
                   className="text-gray-400 hover:text-gray-600"
@@ -93,42 +98,40 @@ export default function StoreProfilesList() {
                   </button>
                 </form>
               </div>
-              <div className="w-full flex flex-col gap-4 pl-16">
-                  <h2 className="text-3xl text-cyan-400 font-semibold">
-                    {detail.profilePost}
-                  </h2>
-                  <div className="mt-2 flex gap-3">
-                    <label className=" text-gray-400 text-xl">Name:</label>
-                    <p className="text-white text-xl">{detail.profileName}</p>
-                  </div>
-
-                  <div className="mt-2 flex gap-3">
-                    <label className=" text-gray-400 font-medium">Tenth</label>
-                    <p className="text-white">
-                      {" "}
-                      <span className="font-semibold">10th (%):</span>{" "}
-                      {detail.profileTenth}
-                    </p>
-                  </div>
-                  <div className="mt-2 flex gap-3">
-                    <label className=" text-gray-400 font-medium">
-                      Twelth:
-                    </label>
-                    <p className="text-white">
-                      {" "}
-                      <span className="font-semibold">12th (%):</span>{" "}
-                      {detail.profileTwelth}
-                    </p>
-                  </div>
+              <div className="w-full flex flex-col gap-2">
+                <h2 className="text-3xl text-cyan-400 font-semibold self-center">
+                  {detail.profilePost}
+                </h2>
+                <div className="mt-2 flex gap-3">
+                  <label className=" text-gray-400 text-xl">Name:</label>
+                  <p className="text-white text-xl">{detail.profileName}</p>
                 </div>
-          
-              <div className="w-full mt-8 flex justify-center">
-                <div className=" flex gap-6 w-[80%] flex-col ">
+
+                <div className="mt-2 flex gap-3">
+                  <label className=" text-gray-400 font-medium">Tenth</label>
+                  <p className="text-white">
+                    {" "}
+                    <span className="font-semibold">10th (%):</span>{" "}
+                    {detail.profileTenth}
+                  </p>
+                </div>
+                <div className="mt-2 flex gap-3">
+                  <label className=" text-gray-400 font-medium">Twelth:</label>
+                  <p className="text-white">
+                    {" "}
+                    <span className="font-semibold">12th (%):</span>{" "}
+                    {detail.profileTwelth}
+                  </p>
+                </div>
+              </div>
+
+              <div className="w-full text-white  flex justify-center">
+                <div className=" flex gap-6 w-full flex-col ">
                   <label className=" text-gray-400 font-medium">Skills:</label>
-                  <div className="flex flex-wrap gap-3 items-center">
+                  <div className="flex flex-wrap gap-3 items-center text-md">
                     {detail.profileSkills.map((skill) => {
                       return (
-                        <span className="px-8 py-2 bg-cyan-950 rounded-lg">
+                        <span className="px-6 py-1.5 bg-cyan-950 rounded-lg">
                           {skill}
                         </span>
                       );
@@ -137,7 +140,7 @@ export default function StoreProfilesList() {
                 </div>
               </div>
 
-              <div className="w-full mt-6 flex justify-center ">
+              <div className="w-full flex justify-center ">
                 <div className="flex  w-[90%] justify-end items-center mt-4">
                   <Link
                     to={`/store/storeProfileDetails/${detail._id}`}
