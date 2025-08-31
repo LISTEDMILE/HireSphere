9f45bb246abb852ccfa0f571d769b347386741e1
listedmile <listedmile@gmail.com>
2025-09-01 01:14:18 +0530

    about employee ui and frontend

--- frontend/src/components/pages/host/aboutEmployee.jsx
+++ frontend/src/components/pages/host/aboutEmployee.jsx
@@ -50,20 +50,26 @@ export default function AboutEmployee() {
   }, [userId]);
 
   return (
-    <div className="flex flex-col items-center w-full bg-black text-white">
+   <div className="w-full min-h-[100vh] flex flex-col items-center ">
+      <div className=" fixed h-[100vh] w-[100vw] top-0 left-0 bg-gradient-to-b from-black via-[#042029] to-[#060a13] z-[-10]"></div>
       <NavHome />
-
-      <h1 className="text-3xl font-bold text-center my-4">Employee Profile</h1>
-
-      <div className="w-[80%] p-12 bg-emerald-950 rounded-lg flex flex-col gap-8">
-        <div className="flex justify-between px-32">
+      <h1 className="relative text-3xl w-full py-4 font-bold text-white text-center">
+        <span className="relative z-10">Employee Profile</span>
+        <span className="absolute inset-0 z-0 bg-gradient-to-r from-transparent via-cyan-500/20 to-transparent animate-shimmer"></span>
+      </h1>
+      <div className="w-full sm:w-[80%]  p-4 sm:p-6 flex flex-col items-center rounded-lg text-white gap-12">
+        <div
+            className="w-full bg-white/5 backdrop-blur-md border border-white/10 
+           rounded-2xl text-wrap shadow-lg flex gap-2 flex-col p-6"
+          >
+           <div className="flex flex-col sm:flex-row justify-around w-full gap-8 mb-6 pb-6 border-b">
         <img src={(formData.profilePicture && formData.profilePicture!==null) ? `${apiURL}${formData.profilePicture}` : "/AlternateProfilePic.png"}
             className="w-[250px] h-[250px] self-center rounded-full mb-6" />
           <div className='flex flex-col justify-center gap-8 items-start'>
             <a className="flex gap-4 items-center hover:underline hover:text-red-100 bg-blue-900
-          hover:bg-[#183b34ab] px-6 py-3 rounded transition-all duration-300 ease-in-out" target={formData.linkedIn ? "_blank" : "_self"} href={formData.linkedIn ? `${formData.linkedIn}`: ""}><span className="text-3xl"> <FaLinkedin/> </span>Linked In  </a>
+          hover:bg-[#183b34ab] px-6 py-3 rounded transition-all duration-300 ease-in-out" target={formData.linkedIn ? "_blank" : "_self"} href={formData.linkedIn ? formData.linkedIn.startsWith("http") ? `${formData.linkedIn}`: "#" : "#"}><span className="text-3xl"> <FaLinkedin/> </span>Linked In  </a>
             <a className="flex gap-4 items-center hover:underline hover:text-red-100  bg-black
-          hover:bg-[#183b34ab] px-6 py-3 rounded transition-all duration-300 ease-in-out" target={formData.gitHub ? "_blank" : "_self"} href={formData.gitHub ? `${formData.gitHub}` : ""}> <span className="text-3xl"> <SiGithub/> </span>Git Hub</a>
+          hover:bg-[#183b34ab] px-6 py-3 rounded transition-all duration-300 ease-in-out" target={formData.gitHub ? "_blank" : "_self"} href={formData.gitHub ? formData.gitHub.startsWith("http") ? `${formData.gitHub}` : "#" : "#"}> <span className="text-3xl"> <SiGithub/> </span>Git Hub</a>
           </div>
           </div>
         <div className="flex flex-col gap-5 mb-7">
@@ -79,16 +85,20 @@ export default function AboutEmployee() {
             return (
               <div className="flex gap-3">
                 <label className="text-gray-400  text-lg">{placeholder}</label>
-                <p className=" text-wrap "> {formData[field]} </p>
+                <p className=" text-wrap overflow-x-scroll sm:overflow-hidden"> {formData[field]} </p>
               </div>
             );
           })}
-        </div>
+          </div>
+          </div>
 
-        <div className="w-full flex flex-wrap gap-8 justify-around">
-          <div className="w-[80%] bg-[#3C2A21] p-4 rounded-lg h-fit">
+<div
+            className="w-full bg-white/5 backdrop-blur-md border border-white/10 
+           rounded-2xl shadow-lg flex gap-2 flex-col p-6"
+          >
+          <div className="w-full border-b p-4 rounded-lg h-fit">
             <div className="w-full flex space-y-2 flex-col">
-              <label className="block text-gray-400 font-medium mb-2">
+              <label className="block text-gray-400 text-lg mb-4">
                 Skills:
               </label>
 
@@ -104,9 +114,9 @@ export default function AboutEmployee() {
             </div>
           </div>
 
-          <div className="w-[80%] h-fit bg-[#3C2A21] p-4 rounded-lg">
+          <div className="w-full h-fit border-b p-4 rounded-lg">
             <div className="w-full flex space-y-2 flex-col">
-              <label className="block text-gray-400 font-medium mb-2">
+              <label className="block text-gray-400 font-medium text-lg mb-4">
                 Achievements:
               </label>
 
@@ -122,9 +132,9 @@ export default function AboutEmployee() {
             </div>
           </div>
 
-          <div className="w-[80%] bg-[#3C2A21] p-4 rounded-lg h-fit">
+          <div className="w-full border-b p-4 rounded-lg h-fit">
             <div className="w-full flex space-y-2 flex-col">
-              <label className="block text-gray-400 font-medium mb-2">
+              <label className="block text-gray-400 font-medium text-lg mb-4">
                 Job Preferences
               </label>
 
@@ -140,9 +150,9 @@ export default function AboutEmployee() {
             </div>
           </div>
 
-          <div className="w-[80%] h-fit bg-[#3C2A21] p-4 rounded-lg">
+          <div className="w-full h-fit border-b p-4 rounded-lg">
             <div className="w-full flex space-y-2 flex-col">
-              <label className="block text-gray-400 font-medium mb-2">
+              <label className="block text-gray-400 font-medium text-lg mb-4">
                 Languages Known
               </label>
               <div className="flex justify-start items-center gap-3 w-full flex-wrap">
@@ -160,19 +170,22 @@ export default function AboutEmployee() {
 
         <div className="flex flex-col gap-3">
           <label className="text-gray-400 text-lg">About User</label>
-          <p className="w-full h-44 p-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-teal-500">
+          <p className="w-full h-fit p-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-teal-500">
             {formData.bio}
           </p>
         </div>
 
-        <div className="flex flex-col p-6 gap-6 w-full rounded-lg bg-amber-950">
-          <h2 className="pl-8 text-lg">Experience:</h2>
+       <div
+            className="w-full bg-white/5 backdrop-blur-md border border-white/10 
+           rounded-2xl shadow-lg flex gap-2 flex-col p-6"
+          >
+          <h2 className=" text-lg">Experience:</h2>
 
-          <div className="w-full flex flex-col items-center gap-8 mt-4 flex-wrap">
+      <div className=" mt-6 flex flex-col  w-full gap-8">
             {formData.experience.map((exp) => {
               return (
-                <div className="bg-cyan-950 flex flex-col p-12 rounded-lg w-[80%] ">
-                  <div className="flex flex-col gap-3">
+               
+                  <div className="bg-[#0034246b] flex flex-col gap-3 p-4 border rounded-lg ">
                     <div className="flex  gap-3 ">
                       <label className="block text-gray-400 text-md ">
                         Company:
@@ -199,19 +212,22 @@ export default function AboutEmployee() {
                       <p className="text-sm text-wrap">{exp.descriptionWork}</p>
                     </div>
                   </div>
-                </div>
+              
               );
             })}
           </div>
         </div>
 
-        <div className="flex flex-col p-6 gap-6 w-full rounded-lg bg-amber-950">
-          <h2 className="pl-8 text-lg">Education:</h2>
+       <div
+            className="w-full bg-white/5 backdrop-blur-md border border-white/10 
+           rounded-2xl shadow-lg flex gap-2 flex-col p-6"
+          >
+          <h2 className="text-lg">Education:</h2>
 
-          <div className="w-full flex justify-around gap-8 mt-4 flex-wrap">
+          <div className="w-full flex flex-col sm:flex-row justify-around gap-8 mt-4 flex-wrap">
             {formData.education.map((edu) => {
               return (
-                <div className="bg-cyan-950 flex flex-col p-12 rounded-lg w-[40%] ">
+                <div className="bg-cyan-950 flex flex-col p-4 rounded-lg w-full sm:w-[40%]  ">
                   <div className="flex flex-col gap-3">
                     <div className="flex  gap-3 ">
                       <label className="block text-gray-400 text-md ">
@@ -245,13 +261,16 @@ export default function AboutEmployee() {
           </div>
         </div>
 
-        <div className="bg-[#3C2A21] p-4 rounded-lg w-full flex flex-col">
+          <div
+            className="w-full bg-white/5 backdrop-blur-md border border-white/10 
+           rounded-2xl shadow-lg flex gap-2 flex-col p-6"
+          >
           <h1 className="text-2xl mb-4">Projects</h1>
 
           <div className="w-full flex flex-col items-center gap-8 mt-12 flex-wrap">
             {formData.projects.map((pro) => {
               return (
-                <div className="bg-cyan-950 flex flex-col rounded-lg w-[80%] p-4">
+                <div className="bg-[#00fff213] flex flex-col rounded-lg w-full sm:w-[80%] p-4">
                   <div className="flex flex-col gap-3">
                     <div className="flex gap-3">
                       <label className="block text-gray-400 text-md ">
@@ -265,12 +284,7 @@ export default function AboutEmployee() {
                       </label>
                       <p className="text-md">{pro.description}</p>
                     </div>
-                    <div className="flex gap-3">
-                      <label className="block text-gray-400 text-md ">
-                        Link:
-                      </label>
-                      <p className="text-md">{pro.link}</p>
-                    </div>
+                   
 
                     <label className="block text-gray-400 text-md ">
                       Technologies Used:
@@ -284,6 +298,15 @@ export default function AboutEmployee() {
                         );
                       })}
                     </div>
+
+                     <div className="flex justify-end w-full gap-3">
+                      
+                      <a
+                        target={pro.link ? "_blank" : "_self"} href={pro.link ? pro.link.startsWith("http") ? `${pro.link}` : "#" : "#"}
+                        className="px-4 py-2 text-md rounded-lg bg-green-600"
+                >Project Link </a>
+                    </div>
+
                   </div>
                 </div>
               );
