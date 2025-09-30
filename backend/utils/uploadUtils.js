d12a388c3760ef7ba13310027fb66e27fb5d0e81
listedmile <listedmile@gmail.com>
2025-10-01 02:45:40 +0530

    restore uploadUtils

---
+++ backend/utils/uploadUtils.js
@@ -0,0 +1,35 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../uploads"));
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9) + ext;
+    cb(null, uniqueName);
+  },
+});
+
+function fileFilter(req, file, cb) {
+  const allowedTypes = /jpeg|jpg|png|gif|webp/;
+  const extname = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  const mimetype = allowedTypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed!"), false);
+  }
+}
+
+// Multer instance
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+});
+
+module.exports = upload;
