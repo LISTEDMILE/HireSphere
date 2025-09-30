16a00ca73cb7cd7221a98a1cfef8ee6b31b98792
listedmile <listedmile@gmail.com>
2025-10-01 03:54:37 +0530

    upload cors removed

--- backend/app.js
+++ backend/app.js
@@ -55,12 +55,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(
-  "/uploads",
-  require("cors")(),
-  express.static(path.join(__dirname, "uploads"))
-);
-
 app.use("/", authRouter);
 app.use("/host", hostRouter);
 app.use("/store", storeRouter);
